78aa7ec7d46a88922b4273685e0b2a91c6b6c8b9
박정현 <pipedline0821@gmail.com>
2021-02-28 18:10:14 +0900

    Add: Pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/base/Header/Header';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <p>
+        HASDF
+      </p>
+    </>
+  );
+};
+
+export default Home;
