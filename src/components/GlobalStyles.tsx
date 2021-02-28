0cb405b4298c159c52c7cbedf72fd0edd7a279f7
박정현 <pipedline0821@gmail.com>
2021-02-28 18:09:02 +0900

    Second Commit

---
+++ src/components/GlobalStyles.tsx
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+  
+  * {
+    box-sizing: border-box;
+    font-size: 16px;
+    font-weight: 400;
+    color: #000;
+  }
+  
+  html,
+  body,
+  #root {
+    width: 100%;
+    height: 100%;
+  }
+  
+  body {
+    font-family: 'Poppins', sans-serif;
+    background-color: #FFF;
+  }
+
+  button {
+    display: flex;
+
+    padding: 8px 12px;
+
+    text-align: center;
+
+    border: none;
+    outline: none;
+
+    &:hover,
+    &:active {
+      border: none;
+      outline: none;
+    }
+  }
+  
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
+export default GlobalStyles;
